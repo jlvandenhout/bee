164cd96d4b9fba605a502ee12d20632aa797aa1b
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-05-09 21:57:24 +0200

    Use node module resolution and update cli

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,22 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    title: 'Bee',
-    url: '/',
-    baseUrl: '/',
-    themes: ['@docusaurus/theme-classic'],
-    plugins: [
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: 'bee',
-                path: path.resolve(__dirname, './docs'),
-                routeBasePath: 'bee',
-                sidebarPath: path.resolve(__dirname, './sidebars.js'),
-                editUrl: 'https://github.com/iotaledger/bee/edit/mainnet-develop',
-                remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
-            }
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "bee",
+        path: path.resolve(__dirname, "./docs"),
+        routeBasePath: "bee",
+        sidebarPath: path.resolve(__dirname, "./sidebars.js"),
+        editUrl: "https://github.com/iotaledger/bee/edit/mainnet-develop",
+        remarkPlugins: [
+          require("remark-code-import"),
+          require("remark-import-partial"),
         ],
+      },
     ],
-    staticDirectories: [path.resolve(__dirname, './static')],
+  ],
+  staticDirectories: [path.resolve(__dirname, "./static")],
 };
